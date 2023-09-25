e8423f4d1e2ae588d53f9c838c8a09214679a42f
kaytee07 <kofi.taylor07@gmail.com>
2023-09-25 16:24:16 +0000

    return object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [department, employees] of Object.entries(employeesList)) {
+    allEmployees[department] = employees;
+  }
+
+  return {
+    allEmployees,
+    getNumberOfDepartments: () => Object.keys(allEmployees).length
+  };
+}
+ 
