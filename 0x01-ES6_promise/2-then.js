7b405b2cd8bc6282eccd2a6878f70e0720fb955f
kaytee07 <kofi.taylor07@gmail.com>
2023-09-27 11:00:42 +0000

    three handlers to function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,5 +3,8 @@ export default function handleResponseFromAPI(promise) {
     status: 200,
     body: 'success',
   }))
-    .catch(() => new Error());
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
