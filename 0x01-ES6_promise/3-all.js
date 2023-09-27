89f21ea4b6ac5c131b28e106976da3be4ec5230b
kaytee07 <kofi.taylor07@gmail.com>
2023-09-27 11:34:27 +0000

    add js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[2].lastName}`);
+    })
+    .catch(() => console.error('Signup system offline'));
+}
