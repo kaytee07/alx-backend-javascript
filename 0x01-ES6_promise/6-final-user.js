1811dcfef83402613e63d25c0e228efdb5d1c1ae
kaytee07 <kofi.taylor07@gmail.com>
2023-09-27 12:41:07 +0000

    correcting syntax error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-	.then((res) => (
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
       res.map((val) => ({
         status: val.status,
         value: val.status === 'fulfilled' ? val.value : String(val.reason),
       }))
-	));
-
+    ));
 }
