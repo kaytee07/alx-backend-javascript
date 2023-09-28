93f36fbf4b8dfc30354ea173a7b30a55411ba548
kaytee07 <kofi.taylor07@gmail.com>
2023-09-28 19:42:39 +0000

    classroom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,13 @@
-class ClassRoom {
-  constructor(maxStudentsSize){
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
