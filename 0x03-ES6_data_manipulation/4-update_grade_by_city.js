df9981306fce02e061302d3116827e207ac389e0
kaytee07 <kofi.taylor07@gmail.com>
2023-10-02 12:48:11 +0000

    grade by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+const updateStudentGradeByCity = (list, city, newGrades) => {
+  return list
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+};
+
+export default updateStudentGradeByCity;
