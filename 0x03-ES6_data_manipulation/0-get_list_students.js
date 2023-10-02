2c03d19b9d8cb88c4b01f1e77d2b0339d44b3474
kaytee07 <kofi.taylor07@gmail.com>
2023-10-02 10:39:15 +0000

    return an array from a function

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 3, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
