dbf9de8e1bdb6ab187672251e25428455195f355
kaytee07 <kofi.taylor07@gmail.com>
2023-10-03 01:33:04 +0000

    get student

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-const getStudentIdsSum = (students) => {
-  return students.reduce((acc, student) => acc + student.id, 0);
-};
+const getStudentIdsSum = (students) => students.reduce((acc, student) => acc + student.id, 0);
 
 export default getStudentIdsSum;
