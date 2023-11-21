60056738228aa982fef1c2eac3b82f4ded1a9d93
kaytee07 <kofi.taylor07@gmail.com>
2023-11-21 16:33:35 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,14 @@
+/**
+* read from input
+*
+*/
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
