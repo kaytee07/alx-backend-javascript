5a61ad71ae191a24940d7e9213fa0a9a10265c1c
kaytee07 <kofi.taylor07@gmail.com>
2023-11-22 05:00:38 +0000

    control

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,10 +1,6 @@
-/**
- * Contains the miscellaneous route handlers.
- * @author Winful Kofi Taylor <https://github.com/Kaytee07>
- */
 class AppController {
   static getHomepage(request, response) {
-    response.status(200).send('Hello Holberton School!');
+    response.send(200, 'Hello Holberton School!');
   }
 }
 
