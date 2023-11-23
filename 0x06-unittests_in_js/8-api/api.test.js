a62b0ab9f141363e0e27ef5d73a22c243da9260b
kaytee07 <kofi.taylor07@gmail.com>
2023-11-23 11:15:38 +0000

    test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -15,13 +15,7 @@ describe("integration test", () => {
     it("correct status code", (done) => {
         request.get("http://localhost:7865", (error, response) => {
             expect(response.statusCode).to.equal(200);
-            done();
-        });
-    });
-
-    it("response with correct message", (done) => {
-        request.get("http://localhost:7865", (error, response, body) => {
-            expect(body).to.equal('Welcome to the payment system');
+	    expect(body).to.equal('Welcome to the payment system');
             done();
         });
     });
