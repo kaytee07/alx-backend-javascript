8190f93fde0615ece4e685daee1eddaffe180f53
kaytee07 <kofi.taylor07@gmail.com>
2023-11-23 10:31:19 +0000

    api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require('express');
 
 
-const app = express()
-const port = 7865
+const app = express();
+const port = 7865;
 
 app.get('/', (req, res) => {
-    res.status(200).send("Welcome to the payment system")
-})
+    res.status(200).send("Welcome to the payment system");
+});
 
 app.listen(port, () => {
-    console.log("API available on localhost port 7865")
-})
+    console.log("API available on localhost port 7865");
+});
 
