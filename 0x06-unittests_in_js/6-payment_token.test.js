f2c57d86fa5744a360842d3119b10d367a7660cd
kaytee07 <kofi.taylor07@gmail.com>
2023-11-23 04:58:42 +0000

    payment token

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const getPaymentTokenFromAPI = require('./6-payment_token')
+const { expected } = require('chai')
+
+
+describe("resolve with correct data", () => {
+    it('resolve with correct data', async () => {
+	getPaymentTokenFromAPI(true).then((result) => {
+	    expect(result).to.equal({data: 'Successful response from the API'});
+	    done();
+	})
+    })
+    
+});
