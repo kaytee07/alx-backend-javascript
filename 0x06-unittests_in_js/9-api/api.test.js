be25012ba085b90795de3651eaa9f8ca934dfff7
kaytee07 <kofi.taylor07@gmail.com>
2023-11-23 07:49:19 +0000

    test status codes

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe("integration test", () => {
+    let server;
+
+    before(() => {
+	server = require('./api');
+    });
+
+    after(() => {
+	server.close;
+    });
+
+    it("correct status code when id is a number", (done) => {
+	request.get("http://localhost:7865/cart/9", (error, response) => {
+	    expect(response.statusCode).to.equal(200);
+	    done();
+	})
+    })
+
+    it("correct status code when id is not a number", (done) => {
+	request.get("http://localhost:7865/card/hello", (error, response, body) => {
+	    expect(response.statusCode).to.equal(404);
+	    done();
+	})
+    })
+    
+})
