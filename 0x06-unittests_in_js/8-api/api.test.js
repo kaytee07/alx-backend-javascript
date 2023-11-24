b66161c83ad0391e3aa49ea5976a9364ee8a9da5
kaytee07 <kofi.taylor07@gmail.com>
2023-11-24 05:21:02 +0000

    api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,22 +1,19 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe("integration test", () => {
-    let server;
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Returns StatusCode: 200 | Body: Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
 
-    before(() => {
-        server = require('./api');
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
-
-    after(() => {
-        server.close;
-    });
-
-    it("correct status code", (done) => {
-        request.get("http://localhost:7865", (error, response, body) => {
-            expect(response.statusCode).to.equal(200);
-	    expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
-    });
+  });
 });
